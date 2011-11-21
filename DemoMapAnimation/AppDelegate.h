f33cb0fca09f3687994c5c1da3697bc8f7a15e24
gaurav sinha <sinhagaurav010@gmail.com>
2011-11-21 14:23:57 +0530

    Initial Commit

---
+++ DemoMapAnimation/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DemoMapAnimation
+//
+//  Created by Rohit Dhawan on 21/11/11.
+//  Copyright (c) 2011 rohit@bosswebtech.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
